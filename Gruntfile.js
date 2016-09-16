51613f422ea8338d5bdd5d3d078bc65b72dec381
safv12 <ing.fran12@gmail.com>
2016-09-16 12:40:56 -0600

    Add karma config file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-karma');
 
   grunt.initConfig({
 
@@ -10,13 +11,25 @@ module.exports = function (grunt) {
           port: 8000,
           hostname: '127.0.0.1',
           base: './app',
-          keepalive: true
+          keepalive: true,
+          livereload: true,
+          middleware: function (connect) {
+            return [
+              require('connect-livereload')(),
+              mountFolder(connect, 'app')
+            ];
+          }
         }
       }
+    },
+
+    karma: {
+      unit: { configFile: './karma.conf.js' }
     }
 
   });
 
   grunt.registerTask('serve', ['connect']);
+  grunt.registerTask('test', ['karma']);
 
 };
