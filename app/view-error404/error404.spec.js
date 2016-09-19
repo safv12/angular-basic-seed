c9ab7016e523ffd52ba8462e48fb2e3de2cf39bf
safv12 <ing.fran12@gmail.com>
2016-09-18 18:50:41 -0600

    Refactor error404.spec.js

--- app/view-error404/error404.spec.js
+++ app/view-error404/error404.spec.js
@@ -1,16 +1,24 @@
 
 describe('Error404 module', function () {
 
-  beforeEach(module('App.error404'));
 
-  it('should Error404Ctrl to be defined', inject(function($controller, $rootScope) {
+  beforeEach(function () {
+    module('App.error404');
 
-    scope = $rootScope.$new();
-    var Error404Ctrl = $controller('Error404Ctrl', { $scope: scope });
+    inject(function($controller, $rootScope) {
+      scope = $rootScope.$new();
+      Error404Ctrl = $controller('Error404Ctrl', { $scope: scope });
+    });
+  });
 
+
+  it('should Error404Ctrl to be defined', function () {
     expect(Error404Ctrl).toBeDefined();
-    expect(scope.errorCode).toBe(404);
+  });
 
-  }));
+
+  it('should errorCode to be 404', function () {
+    expect(scope.errorCode).toBe(404);
+  });
 
 });
