83f83ad6769e51589f9dda14de477e92fc061f13
safv12 <ing.fran12@gmail.com>
2016-09-20 22:05:26 -0500

    Add jshint config to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
+
 module.exports = function (grunt) {
+  'use strict';
 
   // Load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
   require('load-grunt-tasks')(grunt);
@@ -18,11 +20,21 @@ module.exports = function (grunt) {
 
     karma: {
       unit: { configFile: './karma.conf.js' }
+    },
+
+    jshint: {
+      jshintrc: '.jshintrc',
+      all: [
+        'Gruntfile.js',
+        'app/view-*/**/*.js',
+        'app/components/**/*.js'
+      ]
     }
 
   });
 
-  grunt.registerTask('serve', ['connect']);
+  // Grunt tasks
+  grunt.registerTask('serve', ['jshint', 'connect']);
   grunt.registerTask('test', ['karma']);
 
 };
