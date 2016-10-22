22748506a7cd642e0066a02b9c6476cb1528a4c2
safv12 <ing.fran12@gmail.com>
2016-10-22 17:54:40 -0600

    Update routing configuration

--- app/app.module.js
+++ app/app.module.js
@@ -7,10 +7,17 @@
 
   function config($routeProvider) {
     $routeProvider
+      .when('/', {
+        templateUrl: 'view-todo-list/todo-list.html',
+        controller: 'TodoListController',
+        controllerAs: 'todoList'
+      })
       .when('/error404', {
         templateUrl: 'view-error404/error404.html',
-        controller: 'Error404Ctrl'
+        controller: 'Error404Controller',
+        controllerAs: 'error404'
       })
+
       .otherwise({ redirectTo: '/error404' });
   }
 
